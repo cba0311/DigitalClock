90c4e2c76058ed8edba978dfdf5d41b4d80c1c77
Christian X. Betances <christianxavierb@outlook.com>
2022-06-11 18:31:37 -0400

    First commit.

---
+++ app.js
@@ -0,0 +1,37 @@
+function showDate() {
+    let date = new Date();
+
+    const currentDate = date.toDateString();
+
+    document.getElementById("dateDisplay").innerHTML = currentDate;
+    document.getElementById("dateDisplay").textContent = currentDate;
+    setTimeout(showDate, 100000);
+}
+showDate();
+
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours(); // 0 - 23
+    let minutes = date.getMinutes(); // 0 - 59
+    let seconds = date.getSeconds(); // 0 - 59
+    let session = "AM";
+
+    if (hours == 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        session = "PM";
+    }
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    const time = hours + ":" + minutes + ":" + seconds + " " + session;
+
+    document.getElementById("clockDisplay").innerText = time;
+    document.getElementById("clockDisplay").textContent = time;
+    setTimeout(showTime, 1000);
+}
+showTime();
